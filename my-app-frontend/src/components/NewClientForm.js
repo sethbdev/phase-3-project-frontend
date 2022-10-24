9f2188baa39547e3c06b75dfb7029f4454d5bbd2
Seth <seth.bbrendle@gmail.com>
2022-10-24 17:22:23 -0400

    component skeleton

---
+++ my-app-frontend/src/components/NewClientForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewClientForm = () => {
+  return (
+    <div>NewClientForm</div>
+  )
+}
+
+export default NewClientForm
---
+++ my-app-frontend/src/components/NewTherapistForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewTherapistForm = () => {
+  return (
+    <div>NewTherapistForm</div>
+  )
+}
+
+export default NewTherapistForm
