fd118967cbb953c09fde2969853fc8f9beea7d58
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-26 15:13:17 -0400

    MVP

--- my-app-frontend/src/components/AppointmentCard.js
+++ my-app-frontend/src/components/AppointmentCard.js
@@ -6,6 +6,7 @@ function AppointmentCard({ date, time, therapistName, clientName }) {
       <h3>Date: {date}</h3>
       <h3>Time: {time}</h3>
       <h3>Therapist: {therapistName}</h3>
+      <h3>Client: {clientName}</h3>
     </div>
   );
 }
